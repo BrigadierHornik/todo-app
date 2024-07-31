6dcd84db9766f82c238bce75943ff47eebd3af0f
Brigadier <brigadier@brigadier.dev>
2024-07-31 16:40:36 +0200

    Changed the routing so it uses the APIAuthentificator middleware

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,21 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:web');
 
 
-Route::post('/login', [LoginController::class, 'login'])->middleware('web')->name('login');
-Route::post('/hashpassword', [LoginController::class, 'hashpassword']);
+Route::middleware(['auth-api', 'web'])->group(function () {
+    Route::get('/user', function (Request $request) {
+        return response()->json($request->user());
+    });
+    Route::get('/test', [LoginController::class, 'test']);
+    Route::post('/hashpassword', [LoginController::class, 'hashpassword']);
+    
+    Route::get('/logout', function (Request $request) {
+        $request->session()->invalidate();
+        $request->session()->regenerate();
+        return response()->json(['message' => 'Logged out'], 200);
+    });
+    
+});
+
+Route::post('/login', [LoginController::class, 'login'])->middleware("web")->name('login');
