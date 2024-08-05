12e996e2ce8752dac06432d78e4110a4355702e5
Brigadier <brigadier@brigadier.dev>
2024-08-05 03:18:18 +0200

    Forgot migration with the todo commit, adding DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Todo;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'test@test',
+            'password' => bcrypt('test123'),
         ]);
+        User::factory(1)->create();
+
+        Todo::factory(10)->create();
+
     }
 }
